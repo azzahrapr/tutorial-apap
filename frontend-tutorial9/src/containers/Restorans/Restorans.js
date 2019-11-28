5c0d2f65bdea81ef79bab692e8cfb00b83c8c216
Azzahra Putri <azzahraputri05@gmail.com>
2019-11-28 10:39:58 +0700

    feat: coba frontend 3

--- frontend-tutorial9/src/containers/Restorans/Restorans.js
+++ frontend-tutorial9/src/containers/Restorans/Restorans.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
+
 import classes from './Restorans.module.css';
 import Restoran from '../../components/Restoran/Restoran';
 import axios from '../../axios-restoran';
+import Modal from '../../components/UI/Modal/Modal';
+import Button from "../../components/UI/Button/Button";
 
 class Restorans extends Component{
 
@@ -9,7 +12,12 @@ class Restorans extends Component{
         super(props);
         this.state = {
             restorans: [],
-            isLoading: false
+            isCreate: false,
+            isLoading: true,
+            nama: "",
+            alamat: "",
+            nomorTelepon: "",
+            rating: ""
         }
     }
 
@@ -30,6 +38,37 @@ class Restorans extends Component{
         });
     };
 
+    addRestoranHandler = () => {
+        this.setState({ isCreate: true});
+    }
+
+    canceledHandler = () => {
+        this.setState({ isCreate: false});
+    }
+
+    changeHandler = event => {
+        const {name, value} = event.target;
+        this.setState({ [name]: value});
+    }
+
+    submitAddRestoranHandler= event => {
+        event.preventDefault();
+        this.setState({ isLoading: true});
+        this.addRestoran();
+        this.canceledHandler();
+    }
+
+    async addRestoran(){
+        const restoranToAdd = {
+            nama: this.state.nama,
+            alamat: this.state.alamat,
+            nomorTelepon: this.state.nomorTelepon,
+            rating: this.state.rating
+        };
+        await axios.post("/restoran", restoranToAdd);
+        await this.loadRestorans();
+    }
+
     // shouldComponentUpdate(nextProps, nextState){
     //     console.log("shouldComponentUpdate()");
     //     return true
@@ -44,19 +83,77 @@ class Restorans extends Component{
     render() {
         return(
             <React.Fragment>
+                <Modal show={this.state.isCreate || this.state.isEdit}
+                modalClosed={this.canceledHandler}>
+                    {this.renderForm()}
+                </Modal>
                 <div className={classes.Title}>All Restorans</div>
+                <div className={classes.ButtonLayout}>
+                    <button 
+                    className={classes.AddRestoranButton}
+                    onClick={this.addRestoranHandler}>
+                        + Add New Restoran
+                    </button>
+                </div>
+
                 <div className={classes.Restorans}>
-                    {this.state.restorans.map(restoran =>
-                    <Restoran
-                    key={restoran.id}
-                    nama={restoran.nama}
-                    alamat={restoran.alamat}
-                    nomorTelepon={restoran.nomorTelepon}
-                    />
-                    )}
+                    {this.state.restorans && 
+                        this.state.restorans.map(restoran =>
+                                <Restoran
+                                key={restoran.id}
+                                nama={restoran.nama}
+                                alamat={restoran.alamat}
+                                nomorTelepon={restoran.nomorTelepon}
+                                />
+                                )}
                 </div>
             </React.Fragment>
         )
+    }
+
+    renderForm(){
+        return(
+            <form>
+                <input
+                className={classes.Input}
+                name="nama"
+                type="text"
+                placeholder="Nama"
+                value={this.state.nama}
+                onChange={this.changeHandler}
+                />
+                <input
+                className={classes.Input}
+                name="nomorTelepon"
+                type="number"
+                placeholder="Nomor Telepon"
+                value={this.state.nomorTelepon}
+                onChange={this.changeHandler}
+                />
+                <textarea
+                className={classes.TextArea}
+                name="alamat"
+                type="text"
+                placeholder="Alamat"
+                value={this.state.alamat}
+                onChange={this.changeHandler}
+                />
+                <input
+                className={classes.Input}
+                name="rating"
+                type="number"
+                placeholder="Rating"
+                value={this.state.rating}
+                onChange={this.changeHandler}
+                />
+                <Button btnType="Danger" onClick={this.canceledHandler}>
+                    CANCEL
+                </Button>
+                <Button btnType="Success" onClick={this.submitAddRestoranHandler}>
+                    SUBMIT
+                </Button>
+            </form>
+        );
     }
 }
 
