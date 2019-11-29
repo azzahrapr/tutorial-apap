2fb1bcb1b4f189d1b8779b819fd5c68b5bafd88a
Azzahra Putri <azzahraputri05@gmail.com>
2019-11-29 12:55:32 +0700

    feat: frontend coba 6

--- frontend-tutorial9/src/containers/Restorans/Restorans.js
+++ frontend-tutorial9/src/containers/Restorans/Restorans.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
+
 import classes from './Restorans.module.css';
 import Restoran from '../../components/Restoran/Restoran';
-import axios from '../../axios-restoran';
+import axios from '../../axios-restorans';
 import Modal from '../../components/UI/Modal/Modal';
 import Button from "../../components/UI/Button/Button";
 
 
 class Restorans extends Component{
-
     constructor(props){
         super(props);
         this.state = {
@@ -15,12 +15,29 @@ class Restorans extends Component{
             isCreate: false,
             isLoading: true,
             isEdit: false,
+            isUpdate: false,
             nama: "",
             alamat: "",
             nomorTelepon: "",
             rating: "",
+            query: "",
+            filteredRestorans:[]
         }
     }
+    handleInputChange = event => {
+        this.setState({ isUpdate: true});
+        const query = event.target.value;
+
+        this.setState(prevState  => {
+            const filteredRestorans = prevState.restorans.filter(element => {
+                return element.nama.toLowerCase().includes(query.toLowerCase());
+            });
+            return{
+                query,
+                filteredRestorans
+            };
+        });
+    };
 
     componentDidMount() {
         this.loadRestorans();
@@ -102,7 +119,6 @@ class Restorans extends Component{
             nomorTelepon: this.state.nomorTelepon,
             rating: this.state.rating
         };
-
         await axios.put("/restoran/" + this.state.idRestoran, restoranToEdit);
         await this.loadRestorans();
         this.canceledHandler();
@@ -113,25 +129,20 @@ class Restorans extends Component{
         await this.loadRestorans();
     }
 
-
     render() {
-        return(
-            <React.Fragment>
-                <Modal show={this.state.isCreate || this.state.isEdit}
-                    modalClosed={this.canceledHandler}>
-                    {this.renderForm()}
-                </Modal>
-                <div className={classes.Title}>All Restorans</div>
-                <div className={classes.ButtonLayout}>
-                    <button
-                        className={classes.AddRestoranButton}
-                        onClick={this.addRestoranHandler}
-                    >
-                        + Add New Restoran
-                    </button>
-                </div>
-                <div className={classes.Restorans}>
-                    {this.state.restorans &&  
+        const render =  this.state.isUpdate?
+        <div>{this.state.filteredRestorans.map(restoran =>
+                            <Restoran
+                                key={restoran.id}
+                                nama={restoran.nama}
+                                alamat={restoran.alamat}
+                                nomorTelepon={restoran.nomorTelepon}
+                                edit={() => this.editRestoranHandler(restoran)}
+                                delete={() => this.deleteRestoranHandler(restoran.idRestoran)}
+                            />
+                    )} 
+        </div> : <div>
+        {this.state.restorans &&  
                         this.state.restorans.map(restoran =>
                             <Restoran
                                 key={restoran.id}
@@ -141,7 +152,36 @@ class Restorans extends Component{
                                 edit={() => this.editRestoranHandler(restoran)}
                                 delete={() => this.deleteRestoranHandler(restoran.idRestoran)}
                             />
-                    )}  
+                    )} 
+      </div>
+        return(
+            <React.Fragment>
+                
+                <Modal show={this.state.isCreate || this.state.isEdit}
+                    modalClosed={this.canceledHandler}>
+                    {this.renderForm()}
+                </Modal>
+                <div className={classes.Title}>All Restorans</div>
+                <div className="searchForm">
+                    <form>
+                    <input
+                        placeholder="Search for..."
+                        value={this.state.query}
+                        onChange={this.handleInputChange}
+                    />
+                    </form>
+                </div>
+       
+                <div className={classes.ButtonLayout}>
+                    <button
+                        className={classes.AddRestoranButton}
+                        onClick={this.addRestoranHandler}
+                    >
+                        + Add New Restoran
+                    </button>
+                </div>
+                <div className={classes.Restorans}>
+                     {render}
                 </div>
             </React.Fragment>
         );
